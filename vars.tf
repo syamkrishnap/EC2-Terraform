59d40a2ff9e7930f05512be8b11386ca93f4cee1
Syam <skp@its06.inapp.com>
2019-05-17 17:48:59 +0530

    Code sample

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "SOURCE_IP_1" {}
+variable "SOURCE_IP_2" {}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "devops"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "devops.pub"
+}
